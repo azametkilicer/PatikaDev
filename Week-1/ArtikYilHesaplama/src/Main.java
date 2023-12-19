b225b413d7d1a239c321d40dd783eae7aa4e27a6
Azamet KILIÇER <azametkilicer@Azamet-MacBook-Air.local>
2023-12-19 11:51:25 +0300

    Artık Yıl Hesaplama

---
+++ Week-1/ArtikYilHesaplama/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcıdan sorgulamak istediği yılı istiyoruz
+        System.out.print("Yıl Girin: ");
+        int year = scanner.nextInt();
+
+        // artık yıl için bir boolean oluşturuyoruz
+        boolean artikYil = false;
+
+
+        if (year % 4 == 0) {
+
+            if (year % 100 == 0) {
+                artikYil = year % 400 == 0;
+            } else {
+                artikYil = true;
+            }
+        }
+
+
+        if (artikYil) {
+            System.out.println(year + " bir artık yıldır!");
+        } else {
+            System.out.println(year + " bir artık yıl değildir!");
+        }
+    }
+}
--- Week-1/CinZodyagiHesaplama/src/Main.java
+++ Week-1/CinZodyagiHesaplama/src/Main.java
@@ -60,3 +60,4 @@ public class Main {
         System.out.println("Çin Zodyağı Burcunuz: " + burc);
     }
 }
+
